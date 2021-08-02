5dd4a942e79311e524c7759c6f4ccda94885a6a8
fanap_soft_26 <ahmadsajadidev73@gmail.com>
2021-08-02 11:26:37 +0430

    Initial commit

---
+++ book-reader/src/main/java/com/ahmad/book_reader/BookReader.kt
@@ -0,0 +1,12 @@
+package com.ahmad.book_reader
+
+class BookReader {
+
+    fun openBook():String{
+        return "book opened"
+    }
+
+    fun closeBook():String{
+        return "book closed"
+    }
+}
